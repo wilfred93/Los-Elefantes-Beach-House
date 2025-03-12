7106cca81c4dadb96ea5a5a7c0021dbfb05e7ee1
Alfredo <alfredo.lazaro@refurbed.com>
2025-03-12 22:10:48 +0000

    make maps data dynamic

--- components/pages/home/Map.vue
+++ components/pages/home/Map.vue
@@ -10,14 +10,14 @@
         >
           <defs>
             <mask id="anim-mask">
-              <path id="mask-path" d="M546.6 237.846C546.855 207.703 541.2 182.988 528.501 154.499L455.501 188.999C455.501 188.999 444.001 198 426.501 200.5C409.001 203 400.001 197 400.001 197C287.501 114.5 209.501 66.9999 100.501 41.9999L56.4999 145.999" fill="none" stroke="#FFF" stroke-width="3" />
+              <path id="mask-path" :d="locations[selectedLocation].svgPath" fill="none" stroke="#FFF" stroke-width="3" /> 
             </mask>
           </defs>
-            <path d="M546.6 237.846C546.855 207.703 541.2 182.988 528.501 154.499L455.501 188.999C455.501 188.999 444.001 198 426.501 200.5C409.001 203 400.001 197 400.001 197C287.501 114.5 209.501 66.9999 100.501 41.9999L56.4999 145.999" fill="none" stroke="#E5542C" stroke-width="3" stroke-dasharray="7" stroke-linecap="round" mask="url(#anim-mask)"  />
+             <path :d="locations[selectedLocation].svgPath" fill="none" stroke="#E5542C" stroke-width="3" stroke-dasharray="7" stroke-linecap="round" mask="url(#anim-mask)"  /> 
         </svg>
       </div>
       <picture>
-        <img class="w-full rounded-xl border-2 border-elefantes-orange" src="/images/maps/yumbo.png" alt="Los Elefantes Beach House">
+        <img class="w-full rounded-xl border-2 border-elefantes-orange" :src="locations[selectedLocation].mapImg" alt="Los Elefantes Beach House">
       </picture>
     </div>
     <p class="my-6">Playa del Inglés Beach is <span class="text-elefantes-orange">200 meters</span> away from Los Elefantes Beach House</p>
@@ -37,24 +37,30 @@
 </template>
 
 <script setup>
-
 import anime from 'animejs'
+import { ref, nextTick } from 'vue'
+
+const selectedLocation = ref(1)
+const MAPPATH= '/images/maps/'
 
 const locations = [
-  { id: 1, name: 'Playa del Ingles Beach' },
-  { id: 2, name: 'Yumbo Center' },
-  { id: 3, name: 'Las Burras Beach' },
-  { id: 4, name: 'Faro Maspalomas' }
+  { id: 1, name: 'Playa del Ingles Beach', svgPath: 'M190.297 33.6023C222.497 49.5031 240.997 71.5031 240.997 71.5031C240.997 71.5031 225.993 102.506 225.997 115.003C226 127.5 233.497 154.503 231 160.5C228.503 166.497 203.997 203.003 203.997 203.003L265.497 235.503C265.497 235.503 277.5 225.5 288 232.5C298.5 239.5 293 254 293 254C293 254 309.994 256.507 325.997 276.503C342 296.5 354 305.5 354 305.5', mapImg: '/images/maps/playa-del-ingles-beach.png' },
+  { id: 2, name: 'Yumbo Center', svgPath: 'M546.599 237.845C546.855 207.703 541.2 182.988 528.5 154.499L455.5 188.999C455.5 188.999 444 198 426.5 200.5C409 203 400 197 400 197C281 98.9998 185.538 60.9497 98.5379 46.4497C84.9631 89.1179 75.5 109.5 58 148.5', mapImg:'/images/maps/yumbo.png' },
+  { id: 3, name: 'Las Burras Beach', svgPath: 'M0 0' },
+  { id: 4, name: 'Faro Maspalomas', svgPath: 'M0 0' }
 ]
 
-const onLocationClick = (locationId) => {
+const onLocationClick = async (locationId) => {
+  selectedLocation.value = locations.findIndex(loc => loc.id === locationId)
+  
+  await nextTick() // Wait for the DOM to update with the new paths. Avoids the mask not being updated
   anime({
-        targets: '#mask-path',
-        strokeDashoffset: [anime.setDashoffset, 0],
-        easing: 'easeInOutSine',
-        duration: 2000,
-        direction: 'alternate',
-        loop: false
-      })
+    targets: '#mask-path',
+    strokeDashoffset: [anime.setDashoffset, 0],
+    easing: 'easeInOutSine',
+    duration: 2000,
+    direction: 'alternate',
+    loop: false
+  })
 }
 </script> 
