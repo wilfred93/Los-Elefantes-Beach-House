5c0a547ed0745d4fabff1ec49a605dc5a94406ed
Alfredo <alfredo.lazaro@refurbed.com>
2025-04-24 17:52:28 +0100

    add footer to default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,11 +4,13 @@
     <main>
       <slot />
     </main>
+    <Footer />
   </div>
 </template>
 
 <script setup>
 import Header from "~/components/shared/Header.vue";
+import Footer from "~/components/shared/Footer.vue";
 import { useRoute } from "vue-router";
 import { computed } from "vue";
 
