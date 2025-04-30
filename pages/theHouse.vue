167a20287eac673cf3c1b385dfb07b951c2b688f
Alfredo <alfredo.lazaro@refurbed.com>
2025-04-30 18:45:17 +0100

    add what's included section

--- pages/theHouse.vue
+++ pages/theHouse.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="mx-auto max-w-7xl p-4 lg:px-8">
-    <h1 class="primary--title mt-0">The House at a glance</h1>
+    <h1 class="primary--title my-0">The House at a glance</h1>
+    <p class="text-elefantes-green font-montserrat text-center mb-5">
+      Welcome to Los Elefantes Beach House. A beach apartment with a private
+      terrace, pool and garden.
+    </p>
     <section>
       <h2 class="text-elefantes-green font-katibeh text-5xl">Image Gallery</h2>
       <ul
@@ -39,10 +43,105 @@
         </div>
       </div>
     </section>
+    <section class="mt-10">
+      <h2 class="text-elefantes-green font-katibeh text-5xl">
+        What's included?
+      </h2>
+      <ul
+        role="list"
+        class="grid grid-cols-1 gap-x-6 gap-y-8 lg:grid-cols-3 xl:gap-x-8"
+      >
+        <li
+          v-for="client in clients"
+          :key="client.id"
+          class="overflow-hidden rounded-xl border border-gray-200"
+        >
+          <div
+            class="flex items-center gap-x-4 border-b border-gray-900/5 bg-gray-50 p-6"
+          >
+            <img
+              :src="client.imageUrl"
+              :alt="client.name"
+              class="size-12 flex-none rounded-lg bg-white object-cover ring-1 ring-gray-900/10"
+            />
+            <div class="text-sm/6 font-medium text-gray-900">
+              {{ client.name }}
+            </div>
+          </div>
+          <dl class="-my-3 divide-y divide-gray-100 px-6 py-4 text-sm/6">
+            <div class="flex justify-between gap-x-4 py-3">
+              <dt class="text-gray-500">Last invoice</dt>
+              <dd class="text-gray-700">
+                <time :datetime="client.lastInvoice.dateTime">{{
+                  client.lastInvoice.date
+                }}</time>
+              </dd>
+            </div>
+            <div class="flex justify-between gap-x-4 py-3">
+              <dt class="text-gray-500">Amount</dt>
+              <dd class="flex items-start gap-x-2">
+                <div class="font-medium text-gray-900">
+                  {{ client.lastInvoice.amount }}
+                </div>
+                <div
+                  :class="[
+                    statuses[client.lastInvoice.status],
+                    'rounded-md px-2 py-1 text-xs font-medium ring-1 ring-inset',
+                  ]"
+                >
+                  {{ client.lastInvoice.status }}
+                </div>
+              </dd>
+            </div>
+          </dl>
+        </li>
+      </ul>
+    </section>
   </div>
 </template>
 
 <script setup lang="ts">
+const statuses = {
+  Paid: "text-green-700 bg-green-50 ring-green-600/20",
+  Withdraw: "text-gray-600 bg-gray-50 ring-gray-500/10",
+  Overdue: "text-red-700 bg-red-50 ring-red-600/10",
+};
+const clients = [
+  {
+    id: 1,
+    name: "Tuple",
+    imageUrl: "https://tailwindcss.com/plus-assets/img/logos/48x48/tuple.svg",
+    lastInvoice: {
+      date: "December 13, 2022",
+      dateTime: "2022-12-13",
+      amount: "$2,000.00",
+      status: "Overdue",
+    },
+  },
+  {
+    id: 2,
+    name: "SavvyCal",
+    imageUrl:
+      "https://tailwindcss.com/plus-assets/img/logos/48x48/savvycal.svg",
+    lastInvoice: {
+      date: "January 22, 2023",
+      dateTime: "2023-01-22",
+      amount: "$14,000.00",
+      status: "Paid",
+    },
+  },
+  {
+    id: 3,
+    name: "Reform",
+    imageUrl: "https://tailwindcss.com/plus-assets/img/logos/48x48/reform.svg",
+    lastInvoice: {
+      date: "January 23, 2023",
+      dateTime: "2023-01-23",
+      amount: "$7,600.00",
+      status: "Paid",
+    },
+  },
+];
 definePageMeta({
   headerTheme: "dark",
 });
