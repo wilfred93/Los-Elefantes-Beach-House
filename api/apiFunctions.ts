b13c726b129d979e13797722d5591e6524186183
Alfredo <alfredo.lazaro@refurbed.com>
2025-07-05 20:25:24 +0100

    add

--- api/apiFunctions.ts
+++ api/apiFunctions.ts
@@ -4,7 +4,7 @@ import { CONTACT_FORM } from "./endpoints";
 export const sendContactMessage = async (data: any) => {
   const response = await api.post(CONTACT_FORM, data, {
     headers: {
-      Authorization: `Bearer ${Deno.env.get("EDGE_FUNCTION_SECRET")}`,
+      Authorization: `Bearer ${process.env.EDGE_FUNCTION_SECRET}`,
     },
   });
   return response.data;
