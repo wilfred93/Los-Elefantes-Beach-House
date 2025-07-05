c04e666010e0294bd94a98b72b4e390b852cf7a5
Alfredo <alfredo.lazaro@refurbed.com>
2025-07-05 20:12:14 +0100

    add

--- pages/contact.vue
+++ pages/contact.vue
@@ -75,7 +75,6 @@
         <div class="mt-10">
           <button
             @click="onSubmitContactForm"
-            type="submit"
             class="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
             Let's talk
